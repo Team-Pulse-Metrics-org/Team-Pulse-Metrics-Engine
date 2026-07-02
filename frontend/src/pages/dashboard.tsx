f7565dcd6d1707485161e13bf1f549a4617814bf
divakarans <divasundar99@@gmail.com>
2026-07-02 17:18:51 +0530

    dashboard card's skeleton layout completed for all the cards and charts

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,11 +1,30 @@
+import Card from "../components/card";
 
 function Dashboard(){
     return(
-    <>
-      <h1 className="text-3xl font-bold">
+    <div className="bg-slate-950 p-6">
+      <h1 className="text-2xl font-medium text-white pt-8 pl-5">
         Dashboard
       </h1>
-    </>
+      {/* stats section 4 cards */}
+      <div className="grid grid-cols-4 gap-6 mt-8 px-6 h-42">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+      </div>
+      {/* chart section  */}
+      <div className="grid grid-cols-3 gap-6 mt-8 px-6">
+        <Card className="col-span-2 h-[280px]"></Card>
+        <Card className="col-span-1 h-[280px]"></Card>
+        <Card className="col-span-2 h-[280px]"></Card>
+        <Card className="col-span-1 h-[280px]"></Card>
+      </div>
+      {/* Activity section */}
+      <div className="grid grid-cols-4 mt-8 px-6 mb-8">
+        <Card className="col-span-4 h-[460px]"/>
+      </div>
+    </div>
     );
 }
 
