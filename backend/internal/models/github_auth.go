b006e797959c66f98256aed62afd16e748b67405
Fahad Ahmed <fahad838@gmail.com>
2026-07-05 17:54:59 +0530

    Create: Create Request and response body github login endpoint

---
+++ backend/internal/models/github_auth.go
@@ -0,0 +1,23 @@
+package models
+
+type GithubLoginRequest struct {
+	Code string `json:"code" binding:"required"`
+}
+
+type LoginResponse struct {
+	Status string      `json:"status"`
+	Token  string      `json:"token"`
+	User   UserDetails `json:"user"`
+}
+
+type UserDetails struct {
+	ID    string `json:"id"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
+}
+
+type GithubResponse struct {
+	ID    int    `json:"id"`
+	Login string `json:"role"`
+	Email string `email:"email"`
+}
