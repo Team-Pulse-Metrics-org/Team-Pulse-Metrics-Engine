bb3e67ab1c8dc40ee87b2d3ebc0c5e865947502f
Harshitha V <vharshithavenugopal@gmail.com>
2026-07-06 10:08:38 +0530

    metrics querying

---
+++ backend/internal/models/metric.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type MetricsSnapshot struct {
+	ID            uuid.UUID `json:"id"`
+	UserID        uuid.UUID `json:"user_id"`
+	WindowStart   time.Time `json:"window_start"`
+	WindowEnd     time.Time `json:"window_end"`
+	VelocityScore float64   `json:"velocity_score"`
+	TotalCommits  int       `json:"total_commits"`
+	TasksResolved int       `json:"tasks_resolved"`
+	BlockersCount int       `json:"blockers_count"`
+	GeneratedAt   time.Time `json:"generated_at"`
+}
--- backend/sql/queries/metrics.sql
+++
@@ -1,5 +0,0 @@
--- Total commits for a user
-SELECT COUNT(*)
-FROM activities
-WHERE user_id = $1
-  AND activity_type = 'commit';
