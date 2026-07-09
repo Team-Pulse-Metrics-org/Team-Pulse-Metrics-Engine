9fb13b311ffb7b21f1dcab8f590406f8bfb43a38
Fahad Ahmed <fahad838@gmail.com>
2026-07-09 12:55:46 +0530

    Fix: fix markers

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -21,10 +21,6 @@ func main() {
 	}
 	r := gin.Default()
 	r.Use(cors.Default())
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 	r.POST("/api/v1/webhook/github", handlers.HandleWebhook)
 	r.POST("/api/v1/auth/login", handlers.HandleGithubLogin)
 	r.GET("/api/v1/activities", handlers.GetActivities)
