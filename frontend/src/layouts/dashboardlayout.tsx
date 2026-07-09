3d227bf98699d190e16c159ff79dda25b126d0ee
Fahad Ahmed <fahad838@gmail.com>
2026-07-09 12:40:29 +0530

    Fix: indentation

--- frontend/src/layouts/dashboardlayout.tsx
+++ frontend/src/layouts/dashboardlayout.tsx
@@ -1,16 +1,16 @@
 import Sidebar from "../components/sidebar";
 import { Outlet } from "react-router-dom";
 
-function Layout(){
-    return(
-        <div className="flex h-screen">
-            <Sidebar/>
+function Layout() {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
 
-            <main className="flex-1 overflow-y-auto bg-slate-950">
-                <Outlet/>
-            </main>
-        </div>
-    );
+      <main className="flex-1 overflow-y-auto bg-slate-950">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default Layout;
