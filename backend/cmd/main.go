6cb883c88d674745a452ec3d6b9d1b6efc838a14
Fahad Ahmed <fahad838@gmail.com>
2026-07-09 12:35:29 +0530

    Feat: add CORS

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/Sheikh-Fahad-Ahmed/Team-Pulse-Metrics-Engine/internal/database"
+	"github.com/gin-contrib/cors"
 
 	"github.com/Sheikh-Fahad-Ahmed/Team-Pulse-Metrics-Engine/internal/handlers"
 
@@ -19,6 +20,7 @@ func main() {
 		fmt.Println(err)
 	}
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	r.POST("/api/v1/webhook/github", handlers.HandleWebhook)
 	r.POST("/api/v1/auth/login", handlers.HandleGithubLogin)
