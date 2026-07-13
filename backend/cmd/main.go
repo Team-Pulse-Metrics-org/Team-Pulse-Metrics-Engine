76d89a3ae6659f5748997fb1090301e60b57e1cc
Fahad Ahmed <fahad838@gmail.com>
2026-07-13 17:56:43 +0530

    Init: initialize logger with gin server

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Sheikh-Fahad-Ahmed/Team-Pulse-Metrics-Engine/internal/database"
+	"github.com/Sheikh-Fahad-Ahmed/Team-Pulse-Metrics-Engine/internal/middleware"
 	"github.com/gin-contrib/cors"
 
 	"github.com/Sheikh-Fahad-Ahmed/Team-Pulse-Metrics-Engine/internal/handlers"
@@ -13,20 +15,35 @@ import (
 )
 
 func main() {
-
-	database.ConnectDB()
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println(err)
 	}
-	r := gin.Default()
+
+	l := middleware.LogGet()
+	log.Println("app environment", os.Getenv("APP_ENV"))
+	log.Println(os.Getenv("LOG_LEVEL"))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	database.ConnectDB()
+
+	r := gin.New()
+	r.Use(middleware.RequestLogger())
+	r.Use(middleware.Recovery())
 	r.Use(cors.Default())
+
 	r.POST("/api/v1/webhook/github", handlers.HandleWebhook)
 	r.POST("/api/v1/auth/login", handlers.HandleGithubLogin)
 	r.GET("/api/v1/activities", handlers.GetActivities)
-	if err := r.Run(); err != nil {
-		log.Fatalf("server failed to start: %v", err)
+
+	l.Info().
+		Str("port", port).Msgf("Starting Team Pulse Metrics Server on port '%s'", port)
+
+	if err := r.Run(":" + port); err != nil {
+		l.Fatal().Err(err).Msg("server failed to start")
 	}
 }
-
-// removed markers
