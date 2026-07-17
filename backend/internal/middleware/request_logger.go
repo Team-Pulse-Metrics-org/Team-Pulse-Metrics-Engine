e221026f46344801e683284c1933efd28a3a89f0
Fahad Ahmed <fahad838@gmail.com>
2026-07-17 10:56:22 +0530

    Fix: status >= 400 & error log formatting

--- backend/internal/middleware/request_logger.go
+++ backend/internal/middleware/request_logger.go
@@ -48,10 +48,10 @@ func RequestLogger() gin.HandlerFunc {
 			Int("body_size", c.Writer.Size())
 
 		if len(c.Errors) > 0 {
-			event.Str("errors", c.Errors.Last().Err.Error())
+			event.Str("error", c.Errors.Last().Err.Error())
 		}
 
-		if status > 400 {
+		if status >= 400 {
 			event.Str("user-agent", c.Request.UserAgent())
 		}
 
