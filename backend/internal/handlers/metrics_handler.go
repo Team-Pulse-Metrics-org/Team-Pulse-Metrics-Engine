2f9da635a59246532e3069497137ffb70a414fef
Fahad Ahmed <fahad838@gmail.com>
2026-07-24 16:48:25 +0530

    Feat: add goroutines for fetching metrics

--- backend/internal/handlers/metrics_handler.go
+++ backend/internal/handlers/metrics_handler.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"errors"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/Sheikh-Fahad-Ahmed/Team-Pulse-Metrics-Engine/internal/config"
 	"github.com/Sheikh-Fahad-Ahmed/Team-Pulse-Metrics-Engine/internal/models"
@@ -27,21 +29,49 @@ func NewMetricsHandler(q *queries.Queries, cfg *config.Config, log zerolog.Logge
 }
 
 func (h *MetricsHandler) HandleMetrics(c *gin.Context) {
-	weeklyRecords, err := h.q.GetTeamWeeklyMetrics()
-	if err != nil {
-		c.Error(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to collect weekly team metric snapshot"})
+	start := time.Now()
+
+	var wg sync.WaitGroup
+	var (
+		weeklyErr      error
+		monthlyErr     error
+		weeklyRecords  []models.MetricsSnapshot
+		monthlyRecords []models.MetricsSnapshot
+	)
+
+	wg.Go(func() {
+		wStart := time.Now()
+		weeklyRecords, weeklyErr = h.q.GetTeamWeeklyMetrics()
+		h.log.Debug().
+			Dur("duration_ms", time.Since(wStart)).
+			AnErr("error", weeklyErr).
+			Msg("executed GetTeamWeeklyMetrics query")
+	})
+
+	wg.Go(func() {
+		mStart := time.Now()
+		monthlyRecords, monthlyErr = h.q.GetTeamMonthlyMetrics()
+		h.log.Debug().
+			Dur("duration_ms", time.Since(mStart)).
+			AnErr("error", monthlyErr).
+			Msg("executed GetTeamMonthlyMetrics query")
+	})
+
+	wg.Wait()
+	totalDur := time.Since(start)
+
+	if weeklyErr != nil || monthlyErr != nil {
+		h.log.Error().
+			Dur("total_duration_ms", totalDur).
+			Err(weeklyErr).
+			AnErr("monthly_err", monthlyErr).
+			Msg("failed to process metrics request")
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to collect metrics"})
 		return
 	}
 
-	monthlyRecords, err := h.q.GetTeamMonthlyMetrics()
-	if err != nil {
-		c.Error(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to collect monthly team data metric"})
-		return
-	}
 	responsePayload := models.CreateUnifiedResponse(weeklyRecords, monthlyRecords)
-
 	c.JSON(http.StatusOK, responsePayload)
 }
 
